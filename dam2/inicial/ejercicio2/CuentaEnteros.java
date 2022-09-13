3cf060fce55d95bc4bc16ab8c35517eee66f9309
Gabriel Zafra <gab.zafra@gmail.com>
2022-09-13 21:49:18 +0200

    ordenador de numeros enteros

---
+++ dam2/inicial/ejercicio2/CuentaEnteros.java
@@ -0,0 +1,46 @@
+package dam2.inicial.ejercicio2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class CuentaEnteros {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    ArrayList<Integer> listaNumeros = new ArrayList<>();
+    int entrada;
+    boolean flag = true;
+
+    while (flag) {
+      System.out.println("Escriba un entero positivo o -1 para terminar");
+      entrada = input.nextInt();
+      if (entrada > 0) {
+        listaNumeros.add(entrada);
+      } else if (entrada == -1 && listaNumeros.size() > 0) {
+        flag = false;
+      } else {
+        System.err.println("Debe introducir un entero mayor que 0");
+      }
+    }
+    System.out.print("La suma de los números es: ");
+    System.out.println(listaNumeros.stream().mapToInt(Integer::intValue).sum());
+
+    double media = listaNumeros.stream().collect(Collectors.averagingInt(Integer::intValue));
+    System.out.println("La media de los números es: " + media);
+
+    listaNumeros.forEach(num -> System.out.println(
+        "El número " + num + " es " + getNumberPosition(num, media) + " a la media."));
+  }
+
+  private static String getNumberPosition(int num, double media) {
+    if (num < media) {
+      return "menor";
+    } else if (num == media) {
+      return "igual";
+    } else {
+      return "superior";
+    }
+  }
+
+}
